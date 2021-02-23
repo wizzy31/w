7025438ca12f17c38da091da3a0232b38e6b6cc6
wizzy31 <76542649+wizzy31@users.noreply.github.com>
2021-02-22 22:59:43 -0300

    Add files via upload

---
+++ src/cekvip.js
@@ -0,0 +1,15 @@
+const cekvip = () => { 
+	return `           
+──────────────────
+*Nome do bot* :  FIG ZAP
+──────────────────
+        『 *𝐕𝐈𝐏 𝐔𝐒𝐄𝐑* 』
+──────────────────
+• *Status*    : *ATIVO*
+────────────────── 
+*Status Bot:* *Online*
+──────────────────
+
+*AÍ SIM PAE, VOCÊ É UM MEMBRO VIP😎✋`
+}
+exports.cekvip = cekvip
---
+++ src/daftarvip.js
@@ -0,0 +1,21 @@
+const daftarvip = (prefix) => { 
+	return `
+	
+*INFO LISTA VIP :*
+
+-Rp. 0 > Acessar recursos ViP
+-Rp. 0 > Recursos VIP + Insira o bot no seu grupo!
+                       *(APENAS TEAMS)*
+
+*SE QUER REGISTAR VIP :*
+
+*Proprietário do bate-papo BOT :*
+
+_wa.me/558681140024 ou digite *${prefix}owner*_
+
+*NOTA*
+
+*GRUPO DO BOT:*
+https://chat.whatsapp.com/DPWGq1tLCvRGuAbLNJ3u4s `
+}
+exports.daftarvip = daftarvip
